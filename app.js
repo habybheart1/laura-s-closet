2ea01d48c5f377f020a9df5490a4d2d559f46f4f
habybheart1 <akintobiabibat@yahoo.com>
2025-11-28 02:20:42 +0000

    javascipt and product list html file created

---
+++ app.js
@@ -0,0 +1,5 @@
+let iconCart = document.querySelector(".icon-cart");
+let body = document.querySelector("body");
+iconCart.addEventListener('click', () =>{
+    body.classList.toggle('showCart')
+})
